8219b7d3216bf7710403b155f3acb2ff255f5234
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-17 11:42:20 +0200

    Initial Commit

---
+++ FoodRecipeApplication/FoodRecipeApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeApplicationApp.swift
+//  FoodRecipeApplication
+//
+//  Created by Buhle Radzilani on 2024/06/17.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
