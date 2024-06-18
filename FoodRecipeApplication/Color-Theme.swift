4929c73898870008cf2ffb941140b4756ef3e91e
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-18 13:39:49 +0200

    setting up the template for the user interface and also finishing the setup for the color theme.

---
+++ FoodRecipeApplication/Color-Theme.swift
@@ -0,0 +1,21 @@
+//
+//  Color-Theme.swift
+//  FoodRecipeApplication
+//
+//  Created by Buhle Radzilani on 2024/06/18.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color{
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
+
--- FoodRecipeApplication/ContentView.swift
+++ FoodRecipeApplication/ContentView.swift
@@ -8,19 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    let missions = ["Buhle", "Yes", "THree", "Fourr", "Five", "Six", "Seven", "eight", "Ninge", "Ten", "Eleven", "Twelve", "Thirteen", "Fasd"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(missions, id: \.self) {missions in
+                        NavigationLink {
+                        } label: {
+                            VStack{
+//                                Image(missions.image)
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: 100, height: 100)
+//                                    .padding()
+                                
+                                VStack{
+                                    Text("Buhle")
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    
+                                    Text(missions)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                        
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Food items")
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
