9ea60b8d4a67b85d81be2d4fb973a70cbaa7d034
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-18 12:48:33 +0200

    setting up the model

---
+++ FoodRecipeApplication/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  FoodRecipeApplication
+//
+//  Created by Buhle Radzilani on 2024/06/18.
+//
+
+import Foundation
+
+struct Categories: Codable {
+    let categories: [Category]
+}
+
+struct Category: Codable {
+    let idCategory: Int
+    let strCategory: String
+    let strCategoryThumb: String
+    let strCategoryDescription: String
+}
